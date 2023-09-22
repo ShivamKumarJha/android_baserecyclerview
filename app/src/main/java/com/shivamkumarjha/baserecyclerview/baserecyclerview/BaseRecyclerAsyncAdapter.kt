a44e0d338a24bbcafcf76a3a1c7459b3dc61757b
ShivamKumarJha <jha.shivam3@gmail.com>
2023-09-22 20:21:52 +0530

    BaseRecyclerAsyncAdapter: list modify functions
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/baserecyclerview/baserecyclerview/BaseRecyclerAsyncAdapter.kt
+++ app/src/main/java/com/shivamkumarjha/baserecyclerview/baserecyclerview/BaseRecyclerAsyncAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.shivamkumarjha.baserecyclerview.baserecyclerview.util.isValidIndex
 
 abstract class BaseRecyclerAsyncAdapter<T : BaseViewType> :
     RecyclerView.Adapter<DataBoundViewHolder<*>>() {
@@ -63,4 +64,107 @@ abstract class BaseRecyclerAsyncAdapter<T : BaseViewType> :
     fun submitList(items: List<T>) {
         differ.submitList(items)
     }
+
+    private fun modifyList(position: Int? = null, modify: (ArrayList<T>) -> Unit) {
+        val newList = ArrayList(items())
+        if (position != null && !position.isValidIndex(newList)) return
+        modify(newList)
+        submitList(newList)
+    }
+
+    // Adds an item to the end of the list.
+    fun addItem(item: T) {
+        modifyList {
+            it.add(item)
+        }
+    }
+
+    // Inserts an item at a specified position in the list.
+    fun addItem(position: Int, baseViewType: T) {
+        modifyList(position) {
+            it.add(position, baseViewType)
+        }
+    }
+
+    // Adds a list of items to the end of the list.
+    fun addItems(items: List<T>) {
+        modifyList {
+            it.addAll(items)
+        }
+    }
+
+    // Inserts a list of items at a specified position in the list.
+    fun addItems(position: Int, items: List<T>) {
+        modifyList(position) {
+            it.addAll(position, items)
+        }
+    }
+
+    // Clears all items from the list.
+    fun clearItems() {
+        modifyList {
+            it.clear()
+        }
+    }
+
+    // Removes a specific item from the list.
+    fun removeItem(item: T) {
+        modifyList {
+            it.remove(item)
+        }
+    }
+
+    // Removes an item at a specified position from the list.
+    fun removeIndex(position: Int) {
+        modifyList(position) {
+            it.removeAt(position)
+        }
+    }
+
+    // Removes items from the specified start index to the end of the list.
+    fun removeRange(startIndex: Int) {
+        modifyList(startIndex) {
+            val size = it.size
+            it.subList(startIndex, size).clear()
+        }
+    }
+
+    // Removes items within a specified range of indices.
+    fun removeRange(startIndex: Int, endIndex: Int) {
+        modifyList {
+            if (!startIndex.isValidIndex(it)) return@modifyList
+            if (!endIndex.isValidIndex(it)) return@modifyList
+            if (endIndex >= it.size) {
+                it.subList(startIndex, it.size).clear()
+                it.size - startIndex
+            } else {
+                it.subList(startIndex, endIndex + 1).clear()
+                endIndex - startIndex + 1
+            }
+        }
+    }
+
+    // Removes the last item from the list.
+    fun removeLast() {
+        modifyList {
+            removeIndex(it.lastIndex)
+        }
+    }
+
+    // Removes items with a specific baseViewType.
+    fun removeView(@ViewType viewType: Int) {
+        modifyList {
+            it.removeAll { item ->
+                item.baseViewType == viewType
+            }
+        }
+    }
+
+    // Updates an item at a specified position in the list.
+    fun updateItem(position: Int, item: T) {
+        modifyList(position) {
+            it[position] = item
+        }
+    }
+
 }
